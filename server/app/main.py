131f495c0296dccb1029335fa244dae2d52615d9
rodry <rodrymza@gmail.com>
2026-04-06 12:18:52 -0300

    rutas incluidas con cors para permitir futura conexion de front en vite

---
+++ server/app/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from app.database import create_db_and_tables
+from fastapi.middleware.cors import CORSMiddleware
+from app.medicalorder.router import router as medical_router
+from app.systemsettings.router import router as settings_router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+
+app = FastAPI(
+    title="Backend Trabajo Practico 2",
+    lifespan=lifespan,
+    description="API Basica para TP React",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(medical_router)
+app.include_router(settings_router)
