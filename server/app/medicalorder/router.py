0fcd148042cb65654451b06a51190e705d2623b0
rodry <rodrymza@gmail.com>
2026-04-09 07:49:22 -0300

    feature: agregada ruta y funcion para procesar en lote

--- server/app/medicalorder/router.py
+++ server/app/medicalorder/router.py
@@ -1,13 +1,16 @@
-from fastapi import APIRouter, Depends, BackgroundTasks
-from app.medicalorder.service import create_order, get_all
+from fastapi import APIRouter, Depends, BackgroundTasks, Request
+from app.medicalorder.service import create_order, get_all, create_orders_batch_service
 from sqlmodel import Session
 from app.medicalorder.model import (
     MedicalOrderRead,
     MedicalOrderCreate,
     MedicalOrderPagination,
+    BatchOrderResponse,
+    OrderBatchPayload,
 )
 from app.database import get_session
 from app.medicalorder.notifier import evaluate_and_notify
+from typing import List
 
 router = APIRouter(prefix="/orders", tags=["Ordenes"])
 
@@ -37,3 +40,31 @@ def create(
     background_task.add_task(evaluate_and_notify, order.id)
 
     return MedicalOrderRead.from_orm_flat(order)
+
+
+@router.post("/batch", response_model=BatchOrderResponse)
+def create_batch_orders(
+    payload: OrderBatchPayload,
+    background_tasks: BackgroundTasks,
+    session: Session = Depends(get_session),
+):
+    data_list = payload.orders
+    print("Ordenes ", data_list)
+
+    nuevas_ordenes = create_orders_batch_service(session, data_list)
+
+    for order in nuevas_ordenes:
+        background_tasks.add_task(evaluate_and_notify, order.id)
+
+    return {
+        "status": "success",
+        "processed": len(data_list),
+        "created": len(nuevas_ordenes),
+    }
+
+
+@router.post("/studies")
+async def create_studies(request: Request):
+    body = await request.body()
+    print(body)
+    return {}
--- server/app/medicalorder/service.py
+++ server/app/medicalorder/service.py
@@ -27,3 +27,31 @@ def create_order(session: Session, data: MedicalOrderCreate):
     ##Aqui deberiamos llamar a la logica de validacion para completar los campos que faltan
 
     return order
+
+
+def create_orders_batch_service(session: Session, data_list: list[MedicalOrderCreate]):
+    ids_externos = [d.external_id for d in data_list]
+
+    existentes_row = session.exec(
+        select(MedicalOrder.external_id).where(
+            col(MedicalOrder.external_id).in_(ids_externos)
+        )
+    ).all()
+
+    existentes = set(existentes_row)
+    nuevas_instancias = []
+
+    for data in data_list:
+        if data.external_id not in existentes:
+            nueva = MedicalOrder.model_validate(data)
+            session.add(nueva)
+            nuevas_instancias.append(nueva)
+
+    if nuevas_instancias:
+
+        session.commit()
+        # Refrescamos para tener los IDs de Postgres
+        for n in nuevas_instancias:
+            session.refresh(n)
+
+    return nuevas_instancias
